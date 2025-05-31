182ed6d33927d9ab2f9c9ffc76dbfd251e5dc5cd
Aditya Bhandari <adityabhandari264@gmail.com>
2025-06-01 00:31:43 +0530

    some canges

--- Container/dataTypeCamp.js
+++ Container/dataTypeCamp.js
@@ -26,5 +26,45 @@ console.log(null>=0);//beacuse of value conversion it is true
 console.log("2"==2)//true because it convert value and compare it
 console.log("2"===2);//false in this case it is strict check
 
-console.log(aditya)
-console.log(rahul)
+
+
+
+// the DataTypes in javaScript
+//there are two types of datatype primitive and non primitive
+//primitive are 1 string 2 boolean 3 number 4null 5 undifine 6 symbol 7 bigint
+//non primitive are 1 arrray 2 object 3 function
+
+const score=100
+console.log(typeof score);
+const ame="aditya"
+console.log(typeof ame);
+const correct=false
+console.log(typeof correct);
+let level;
+console.log(typeof level);
+let age =null
+console.log(typeof age);
+let id=Symbol(123)
+let anotherId=Symbol(123)
+console.log(id===anotherId);//because id have unique value
+const bigNumber=124356644635236365645656n
+console.log(typeof bigNumber);
+
+
+const hero=["ironman" , "batman", "catman"]
+console.log(typeof hero)
+
+const movie={
+    name:"sholye",
+    date: 30 
+}
+console.log(typeof movie)
+const aditya= function(){
+    console.log("hello world");
+    
+}
+console.log(typeof aditya);
+
+
+
+
