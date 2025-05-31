c9d1a324bd9b033f3b6157bb04646732a8d176d0
Aditya Bhandari <adityabhandari264@gmail.com>
2025-05-31 09:22:17 +0000

    adding dataconversion

---
+++ Container/datatypeCon.js
@@ -0,0 +1,19 @@
+let score ;
+console.log(score)  //this is undefiend because variable declairebut value not 
+let number="33"
+console.log(typeof number);  //convertion of datatype number into string
+// let Score=23
+// let valueInNumber=Number(Score)
+// console.log(typeof valueInNumber); 
+
+// let num ="33abc"
+// let valueInNumber=Number(num)
+// console.log( valueInNumber);  //it give type number but value not an number
+
+let any = ""
+let BooleanAny=Boolean(any)
+console.log(BooleanAny );   // 1 give true and false gives 0 in boolean empty string also convert in false if i write something in side the string it gives true
+
+
+
+
