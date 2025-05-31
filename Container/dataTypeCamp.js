4dd09426f4fae7f74f0837a21d515bf25066aa01
Aditya Bhandari <adityabhandari264@gmail.com>
2025-05-31 23:52:03 +0530

    datacamparison

---
+++ Container/dataTypeCamp.js
@@ -0,0 +1,27 @@
+let str1=23
+let str2=55
+let str3=(str1+str2)
+console.log(str3);
+
+let num=("2"+5+6)
+console.log(num)
+
+let num2=(3+4+"5")
+console.log(num2);
+
+console.log(+true );//it gives 1
+console.log(+"")//it gives 0
+
+
+console.log(2>1)//true
+console.log(2>=1)//true
+console.log(2<1)//false
+console.log(2==1)//false
+console.log(2!=1)//true
+console.log("2">1)  //js convert string in a number
+console.log(null>0);//because null is empty datatype
+console.log(null==0)//""
+console.log(null>=0);//beacuse of value conversion it is true
+
+console.log("2"==2)//true because it convert value and compare it
+console.log("2"===2);//false in this case it is strict check
